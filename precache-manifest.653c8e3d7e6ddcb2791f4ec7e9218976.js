40693f22f33a74186b0886b9f3d1da3f302f5e6d
Ben Eater <ben@eater.net>
2019-12-15 13:04:31 -0800

    Updates

---
+++ precache-manifest.653c8e3d7e6ddcb2791f4ec7e9218976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e83dbcbed3b298feea55241c6f6c21",
+    "url": "/taxi-poisson/index.html"
+  },
+  {
+    "revision": "4024ec181c6d7b563910",
+    "url": "/taxi-poisson/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d954ca6e40fceda0aeb9",
+    "url": "/taxi-poisson/static/css/main.88ecea28.chunk.css"
+  },
+  {
+    "revision": "4024ec181c6d7b563910",
+    "url": "/taxi-poisson/static/js/2.91d1d712.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/taxi-poisson/static/js/2.91d1d712.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d954ca6e40fceda0aeb9",
+    "url": "/taxi-poisson/static/js/main.c9b1d917.chunk.js"
+  },
+  {
+    "revision": "5d6f6d4c95e131ac8a89",
+    "url": "/taxi-poisson/static/js/runtime-main.26230ace.js"
+  }
+]);
